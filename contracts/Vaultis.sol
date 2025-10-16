2b9125a0442e3483dc75424251dad1d29fe4f7bb
Arjen Hammond <arjenhammond@gmail.com>
2025-10-16 04:29:13 +0000

    feat: Add initial Vaultis.sol contract with Ownable

---
+++ contracts/Vaultis.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Vaultis is Ownable {
+    // Contract logic will go here
+}
